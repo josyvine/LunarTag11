932c4dcd3e34dd270f9c90234155540f8f9c25b1
josyvine <64621103+josyvine@users.noreply.github.com>
2025-11-19 06:14:38 +0530

    Update LunarTagApplication.java

--- app/src/main/java/com/lunartag/app/LunarTagApplication.java
+++ app/src/main/java/com/lunartag/app/LunarTagApplication.java
@@ -4,7 +4,6 @@ import android.app.Application;
 
 // Import the core FirebaseApp class
 import com.google.firebase.FirebaseApp;
-import com.google.firebase.messaging.FirebaseMessaging;
 
 /**
  * The custom Application class for Lunar Tag.
@@ -12,17 +11,12 @@ import com.google.firebase.messaging.FirebaseMessaging;
  */
 public class LunarTagApplication extends Application {
 
-    private static final String FCM_TOPIC_FEATURE_TOGGLES = "feature_toggles";
-
     @Override
     public void onCreate() {
         super.onCreate();
 
         // Manually initialize Firebase. This MUST be the first Firebase call.
+        // This allows Remote Config to function correctly.
         FirebaseApp.initializeApp(this);
-
-        // Now that Firebase is initialized, it is safe to use its services.
-        // Subscribe to the remote feature toggle topic. This is a "fire and forget" call.
-        FirebaseMessaging.getInstance().subscribeToTopic(FCM_TOPIC_FEATURE_TOGGLES);
     }
 }
